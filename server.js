7dd6156bffd9629567111117009e506cedf0c34e
steini54 <info@syntext.ch>
2026-02-23 12:28:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -76,6 +76,6 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0." => {
   console.log("Server läuft auf Port " + PORT);
 });
