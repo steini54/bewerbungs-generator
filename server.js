ce191df4be2f53e8abf4adfd8360afb714845ae6
steini54 <info@syntext.ch>
2026-02-23 10:29:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const puppeteer = require("puppeteer");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json({ limit: "10mb" }));
